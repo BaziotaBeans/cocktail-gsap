abfb134176124c26ea1719b87c48ba5053d3941e
BaziotaBeans <1000017079@ucan.edu>
2025-11-09 16:18:48 +0100

    feat: setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center">
+    <div className="flex-center h-screen">
       <h1 className="text-4xl font-bold text-red-500">Hello World</h1>
     </div>
   );
