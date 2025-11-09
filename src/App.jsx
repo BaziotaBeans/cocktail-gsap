5990a4ee07ed320239b4b9fe784b639241a38383
BaziotaBeans <1000017079@ucan.edu>
2025-11-09 16:23:10 +0100

    feat: add nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-4xl font-bold text-red-500">Hello World</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
