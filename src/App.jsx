abceb131455020dbb3bd926bcbe9bdc2cf6fc421
BaziotaBeans <1000017079@ucan.edu>
2025-11-09 16:03:27 +0100

    feat: configure gsap base lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div>App</div>;
+};
 
-export default App
+export default App;
