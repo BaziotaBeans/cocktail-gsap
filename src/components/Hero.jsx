0d30bd0fb1b4797729e8e4c5af6a0d0176626cc0
BaziotaBeans <1000017079@ucan.edu>
2025-11-09 16:46:50 +0100

    feat: add hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <section id="hero" className="noisy"></section>
+    </>
+  );
+};
+
+export default Hero;
