1880b4c93bd40036ca0ea1519b8d358f493786b7
BaziotaBeans <1000017079@ucan.edu>
2025-11-12 19:14:44 +0100

    feat: update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import React from "react";
-import { navLinks } from "../../constants";
-import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../constants";
 
 const Navbar = () => {
   useGSAP(() => {
